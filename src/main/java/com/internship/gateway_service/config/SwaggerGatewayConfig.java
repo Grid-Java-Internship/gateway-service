96c96a19e00e9525b4b45c80f4d2e4650b971133
Milan Stošić <milanstosic00@gmail.com>
2025-04-28 12:25:33 +0200

    Kubernetes deployment config

--- src/main/java/com/internship/gateway_service/config/SwaggerGatewayConfig.java
+++ src/main/java/com/internship/gateway_service/config/SwaggerGatewayConfig.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 
 import java.util.HashSet;
