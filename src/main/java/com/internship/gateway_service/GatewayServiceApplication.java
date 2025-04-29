49bf2b84fd8cf9ae3292728a0c95cc8ba78b7c35
Milan Stošić <milanstosic00@gmail.com>
2025-04-29 10:42:54 +0200

    Added eureka annotation

--- src/main/java/com/internship/gateway_service/GatewayServiceApplication.java
+++ src/main/java/com/internship/gateway_service/GatewayServiceApplication.java
@@ -1,9 +1,11 @@
 package com.internship.gateway_service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
